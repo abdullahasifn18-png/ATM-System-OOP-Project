74b3f9f8ad555b43ee94ca248aa1d8b679c662b9
M-Sarim10 <sarim10106@gmail.com>
2026-04-24 18:09:35 +0500

    Adding Account header structure

---
+++ Account.h
@@ -0,0 +1,25 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include<iostream>
+using namespace std;
+
+class Transaction;
+class Account {
+private:
+    string accountNumber;
+    double balance;
+    Transaction* transactions[50];  
+    int transactionCount;
+public:
+    Account(string accNo, double initialBalance);
+    ~Account();
+    void deposit(double amount);
+    bool withdraw(double amount);
+    bool transfer(Account* target, double amount);
+    double getBalance();
+    void printMiniStatement();
+    void addTransaction(Transaction* t);
+    void saveToFile();
+    void loadFromFile();
+};
+#endif
